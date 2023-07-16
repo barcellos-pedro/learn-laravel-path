dd1f3c6912f6d2b39a0b21e363c5f5bee32f528c
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-07-16 20:56:05 -0300

    feat: episode 16 and 17

--- php-for-beginners/section-2/index.php
+++ php-for-beginners/section-2/index.php
@@ -1,4 +1,21 @@
 <?php
 
 require "functions.php";
-require "router.php";
+//require "router.php";
+
+// connect to our MySQL databse.
+$dsn = "mysql:host=127.0.0.1;port=3306;dbname=myapp;user=root;password=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<ul>";
+echo "<h1>Blog Posts</h1>";
+foreach ($posts as $post) {
+    echo "<li>{$post["title"]}</li>";
+}
+echo "</ul>";
