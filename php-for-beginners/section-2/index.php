0b6b82540e456bdc0a4bf6679b8300156c354c57
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-07-17 19:17:35 -0300

    feat: episode 18

--- php-for-beginners/section-2/index.php
+++ php-for-beginners/section-2/index.php
@@ -1,17 +1,11 @@
 <?php
 
 require "functions.php";
+require "Database.php";
 //require "router.php";
 
-// connect to our MySQL databse.
-$dsn = "mysql:host=127.0.0.1;port=3306;dbname=myapp;user=root;password=root;charset=utf8mb4";
-
-$pdo = new PDO($dsn);
-
-$statement = $pdo->prepare("SELECT * FROM posts");
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$db = new Database();
+$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
 
 echo "<ul>";
 echo "<h1>Blog Posts</h1>";
