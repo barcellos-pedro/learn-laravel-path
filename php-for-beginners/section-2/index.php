026c57a6efa5a65dc23f5d77621c222e0b7667b1
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-07-17 23:10:43 -0300

    feat: episode 20

--- php-for-beginners/section-2/index.php
+++ php-for-beginners/section-2/index.php
@@ -5,9 +5,15 @@ require "Database.php";
 //require "router.php";
 
 $config = require('config.php');
-
 $db = new Database($config['database']);
-$posts = $db->query("SELECT * FROM posts")->fetchAll();
+
+// fetch single post from URL query params
+// $id = $_GET['id']; // /?id=1
+// $query = "SELECT * FROM posts WHERE id = ? (or) :id";
+// $post = $db->query($query, [$id] (or) ['id' => $id])->fetch(); // fetch single post, not an array
+
+$query = "SELECT * FROM posts";
+$posts = $db->query($query)->fetchAll();
 
 echo "<ul>";
 echo "<h1>Blog Posts</h1>";
