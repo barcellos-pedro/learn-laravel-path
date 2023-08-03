6f08eec4f30d6d70d59c85a341f8f5bd509e6f4b
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-08-03 19:10:31 -0300

    feat: episode 8 - start series

---
+++ laravel-8-from-scratch/blog/resources/views/posts.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <title>Blog</title>
+</head>
+
+<body>
+    <article>
+        <h1>
+            <a href="/post/my-first-post">My first post</a>
+        </h1>
+        <p>
+            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+            lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+            lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+        </p>
+    </article>
+
+    <article>
+        <h1>
+            <a href="/post/my-second-post">My second post</a>
+        </h1>
+        <p>
+            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+            lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+            lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+        </p>
+    </article>
+
+    <article>
+        <h1>
+            <a href="/post/my-third-post">My third post</a>
+        </h1>
+        <p>
+            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+            lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+            lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+        </p>
+    </article>
+</body>
+
+</html>
