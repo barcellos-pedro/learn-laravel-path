ca8cce45f13ae805d97302120df5741dc140aa49
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-08-07 20:19:44 -0300

    feat: episode 22

--- laravel-8-from-scratch/blog/app/Models/Post.php
+++ laravel-8-from-scratch/blog/app/Models/Post.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    /** 
+     * Enable fields to create a new Post
+     * e.g: using Post::create(['title'])...
+     * */
+    protected $fillable = [
+        'title',
+        'excerpt',
+        'body'
+    ];
+
+    /**
+     * Enable mass assignment on all fields,
+     * except those listed below.
+     * Opposite of $fillable;
+     */
+    protected $guarded = ['id'];
 }
