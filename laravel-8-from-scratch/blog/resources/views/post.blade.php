9a094e01e817c9de59c3d1e99c3afdcf0f71445f
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-08-07 20:02:05 -0300

    feat: episode 21

--- laravel-8-from-scratch/blog/resources/views/post.blade.php
+++ laravel-8-from-scratch/blog/resources/views/post.blade.php
@@ -1,9 +1,7 @@
 <x-layout>
     <article>
         <h1>{{ $post->title }}</h1>
-
-        <!-- Do not scape the content -->
-        <p>{!! $post->body !!}</p>
+        <p>{{ $post->body }}</p>
     </article>
 
     <a href="/">Go back</a>
