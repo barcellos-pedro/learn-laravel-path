b16fed970653b214fe392f0c5cb3931d0b2d067b
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-08-07 20:44:13 -0300

    refactor: Post

--- laravel-8-from-scratch/blog/app/Models/Post.php
+++ laravel-8-from-scratch/blog/app/Models/Post.php
@@ -9,34 +9,10 @@ class Post extends Model
 {
     use HasFactory;
 
-    /** 
-     * Enable fields to create a new Post
-     * e.g: using Post::create(['title'])...
-     * */
     protected $fillable = [
         'title',
         'excerpt',
         'body',
         'slug'
     ];
-
-    /**
-     * Enable mass assignment on all fields,
-     * except those listed below.
-     * Opposite of $fillable;
-     */
-    // protected $guarded = ['id'];
-
-    /**
-     * Set default attribute to use on route model binding
-     * replacing the default (id)
-     * 
-     * Nice to use when there is multiple routes using
-     * that key /post/{post:<field>}
-     * So we define in only one place, the model
-     */
-    public function getRouteKeyName()
-    {
-        return 'slug';
-    }
 }
--- laravel-8-from-scratch/blog/routes/web.php
+++ laravel-8-from-scratch/blog/routes/web.php
@@ -3,28 +3,13 @@
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('posts', [
         'posts' => Post::all()
     ]);
 });
 
-/**
- * Route model binding
- * wildcard must be equal to variable name
- */
-Route::get('/posts/{post}', function (Post $post) {
+Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', [
         'post' => $post
     ]);
