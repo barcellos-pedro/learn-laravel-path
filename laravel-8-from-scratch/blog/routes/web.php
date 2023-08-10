59cbc49bd9e70f5683fc21f7b85ed535eabe6a58
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-08-10 15:33:28 -0300

    feat: episode 34

--- laravel-8-from-scratch/blog/routes/web.php
+++ laravel-8-from-scratch/blog/routes/web.php
@@ -8,9 +8,8 @@ use Illuminate\Support\Facades\Route;
 /** Get all Posts */
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::latest()
-            ->with(['category', 'author'])
-            ->get()
+        'posts' => Post::latest()->with(['category', 'author'])->get(),
+        'categories' => Category::all()
     ]);
 });
 
@@ -31,13 +30,16 @@ Route::get('/categories', function () {
 /** Get all Posts by Category */
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts->load(['category', 'author'])
+        'posts' => $category->posts->load(['category', 'author']),
+        'categories' => Category::all(),
+        'currentCategory' => $category
     ]);
 });
 
 /** Get all Posts by User */
 Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts->load(['category', 'author'])
+        'posts' => $author->posts->load(['category', 'author']),
+        'categories' => Category::all()
     ]);
 });
