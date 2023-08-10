88d65ea4b9f154b5ea58d64590b9e298795b3428
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-08-10 16:28:06 -0300

    feat: episode 36

--- laravel-8-from-scratch/blog/resources/views/components/dropdown.blade.php
+++ laravel-8-from-scratch/blog/resources/views/components/dropdown.blade.php
@@ -1,11 +1,11 @@
 <div x-data="{ show: false }" @click.outside="show = false" @keyup.escape="show = false" class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
     {{-- Trigger --}}
-    <div @click="show = !show">
+    <div @click="show = !show" class="w-full">
         {{ $trigger }}
     </div>
 
     {{-- Links --}}
-    <div x-show="show" class="py-3 absolute z-10 top-10 bg-gray-100 w-full lg:w-44" style="display: none;">
+    <div x-show="show" class="max-h-52 overflow-auto py-3 absolute z-10 top-10 bg-gray-100 w-full lg:w-44" style="display: none;">
         {{ $slot }}
     </div>
 </div>
